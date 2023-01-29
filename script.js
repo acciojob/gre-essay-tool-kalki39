b2022255d536b23a3ce96d88a1efdeec4e457e1f
kalki39 <120099754+kalki39@users.noreply.github.com>
2023-01-29 22:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let input=document.getElementById("evaluatedText");
+let words=document.getElementById("wordCount");
+
+function fun() {
+	words.innerHTML=input.value.length;
+}
+// input.addEventListener("onkeyup" ,()=>{
+// 	words.innerHTML=input.value.length;
+// })
