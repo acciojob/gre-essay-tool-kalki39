af72c3e03543d2f957fdcfbfd01386e4810e6a02
kalki39 <120099754+kalki39@users.noreply.github.com>
2023-01-29 22:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let input=document.getElementById("evaluatedText");
 let words=document.getElementById("wordCount");
 
 function fun() {
-	// if(input.value==""){
-	// 	words.innerText=0;
-	// }
+	if(input.value==""){
+		words.innerText=0;
+	}
 	words.innerHTML=Number(input.value.length);
 }
 // input.addEventListener("onkeyup" ,()=>{
